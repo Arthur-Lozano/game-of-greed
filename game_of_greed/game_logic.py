f5c1615d28974f2c8e482bde7015904ee5f9bc74
Arthur Lozano <chillout885@gmail.com>
2021-12-22 19:39:21 -0800

    add calculate score functions

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -8,3 +8,10 @@ class GameLogic:
         for _ in range(rolled_dice):
             dice_list.append(random.randint(1,6))
         return tuple(dice_list)
+
+    @staticmethod
+    def calculate_score(calculate_score)
+    """
+    Dice is a tuple of integers that represent a dice roll
+    """
+        
