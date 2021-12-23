f1330fbf8eaf0c9f77adb213aef6bd82dd0fc41f
Wenhao Piao <wenhaopi@amazon.com>
2021-12-22 21:57:12 -0800

    Implement calculate_score method

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 
 
 class GameLogic:
@@ -15,35 +16,63 @@ class GameLogic:
         Dice is a tuple of integers that represent a dice roll
         """
         score_table = {
-            (1,): 100,
-            (5,): 50,
-            (1, 5): 150,
-            (1, 1): 200,
-            (5, 5): 100,
-            (5, 1): 150,
             (1, 2, 3, 4, 5, 6): 1500,
             (6, 6, 6): 600,
             (6, 6, 6, 6): 1200,
-            (6, 6, 6, 6, 6): 2400,
-            (6, 6, 6, 6, 6, 6): 4800,
+            (6, 6, 6, 6, 6): 1800,
+            (6, 6, 6, 6, 6, 6): 2400,
+            (5,): 50,
+            (5, 5): 100,
             (5, 5, 5): 500,
             (5, 5, 5, 5): 1000,
-            (5, 5, 5, 5, 5): 2000,
-            (5, 5, 5, 5, 5, 5): 4000,
+            (5, 5, 5, 5, 5): 1500,
+            (5, 5, 5, 5, 5, 5): 2000,
             (4, 4, 4): 400,
             (4, 4, 4, 4): 800,
-            (4, 4, 4, 4, 4): 1600,
-            (4, 4, 4, 4, 4, 4): 3200,
+            (4, 4, 4, 4, 4): 1200,
+            (4, 4, 4, 4, 4, 4): 1600,
             (3, 3, 3): 300,
             (3, 3, 3, 3): 600,
-            (3, 3, 3, 3, 3): 1200,
-            (3, 3, 3, 3, 3, 3): 2400,
+            (3, 3, 3, 3, 3): 900,
+            (3, 3, 3, 3, 3, 3): 1200,
             (2, 2, 2): 200,
             (2, 2, 2, 2): 400,
-            (2, 2, 2, 2, 2): 800,
-            (2, 2, 2, 2, 2, 2): 1600,
+            (2, 2, 2, 2, 2): 600,
+            (2, 2, 2, 2, 2, 2): 800,
+            (1,): 100,
+            (1, 1): 200,
             (1, 1, 1): 1000,
             (1, 1, 1, 1): 2000,
-            (1, 1, 1, 1, 1): 4000,
+            (1, 1, 1, 1, 1): 3000,
+            (1, 1, 1, 1, 1, 1): 4000,
         }
-        return score_table[dice_roll]
+
+        # sorted = tuple(list(dice_roll).sort())
+        sorted = list(dice_roll)
+        sorted.sort()
+        sorted_tuple = tuple(sorted)
+        if sorted_tuple in score_table:
+            return score_table[sorted_tuple]
+        else:
+            parsed_input = Counter(dice_roll)
+            # to do: group the same numbers in the tuple
+            groups = []
+            for i in parsed_input:
+                group = []
+                for j in range(parsed_input[i]):
+                    group.append(i)
+                    # [3, 3, 3]
+                groups.append(tuple(group))
+            result = 0
+            for group in groups:
+                if group in score_table:
+                    result += score_table[group]
+                # else:
+                #     result += 0
+            print(result)
+            return result
+
+
+# Testing
+input = (3, 3, 3, 2, 2, 2, 2, 1)
+GameLogic.calculate_score(input)
--- tests/test_calculate_score.py
+++ tests/test_calculate_score.py
@@ -88,28 +88,28 @@ def test_four_of_a_kind():
     assert actual == expected
 
 
-@pytest.mark.skip("Pending")
+# @pytest.mark.skip("Pending")
 def test_five_of_a_kind():
     actual = GameLogic.calculate_score((2, 2, 2, 2, 2))
     expected = 600
     assert actual == expected
 
 
-@pytest.mark.skip("Pending")
+# @pytest.mark.skip("Pending")
 def test_six_of_a_kind():
     actual = GameLogic.calculate_score((2, 2, 2, 2, 2, 2))
     expected = 800
     assert actual == expected
 
 
-@pytest.mark.skip("Pending")
+# @pytest.mark.skip("Pending")
 def test_six_ones():
     actual = GameLogic.calculate_score((1, 1, 1, 1, 1, 1))
     expected = 4000
     assert actual == expected
 
 
-@pytest.mark.skip("Pending")
+# @pytest.mark.skip("Pending")
 @pytest.mark.parametrize(
     "test_input,expected",
     [
@@ -156,7 +156,7 @@ def test_six_ones():
         ((1, 1, 1, 2, 2, 2), 1200),
     ],
 )
-@pytest.mark.skip("Pending")
+# @pytest.mark.skip("Pending")
 def test_all(test_input, expected):
     actual = GameLogic.calculate_score(test_input)
     assert actual == expected
